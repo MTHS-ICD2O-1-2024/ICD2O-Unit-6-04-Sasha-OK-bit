4b2323fabde6ac70f8afd166ddb883c635ffb0f4
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-05-23 16:56:20 +0000

    Done with

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Sasha Vorontsov
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+
+  const params = new URLSearchParams(document.location.search)
+
+  const aBase
+}
